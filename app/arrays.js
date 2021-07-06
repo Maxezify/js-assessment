b69040b5dd5f83d4f7ae58c5546d90327f4780c7
Cowlor <gillet.maxence.isart@gmail.com>
2021-07-06 15:06:01 +0200

    Commencement
    Debut du test

--- app/arrays.js
+++ app/arrays.js
@@ -3,15 +3,55 @@ exports = typeof window === 'undefined' ? global : window;
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
 
+if (Array.prototype.indexOf(2)) {
+
+  return arr.indexOf(item);
+
+}
+
   },
+
+
 
   sum: function(arr) {
+ 
+  /*let sumArr = arr.reduce(function(a,b){
 
+    return a + b;
+
+  });
+
+  return sumArr;*/
+
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+
+  result += arr[i]; 
+
+  }
+
+  return result;
+  
   },
+
+
 
   remove: function(arr, item) {
 
+    let remove = arr.length;
+
+    for (let i = 0; i < arr.length; i++) {
+  
+    arr.splice(i, remove);
+  
+    }
+  
+    return arr;
+
   },
+
+
 
   removeWithoutCopy: function(arr, item) {
 
@@ -19,29 +59,88 @@ exports.arraysAnswers = {
 
   append: function(arr, item) {
 
+    arr.push(item);
+
+    return arr;
+
   },
+
+
+
 
   truncate: function(arr) {
 
+    /*
+    
+    ENLEVE SEULEMENT LA VALEUR
+    
+    arr.slice(0,-1);
+
+    return arr;*/
+
+    arr.pop();
+
+    return arr;
+
   },
+
+
 
   prepend: function(arr, item) {
 
+    arr.unshift(item);
+
+    return arr;
+
   },
+
+
 
   curtail: function(arr) {
 
+    arr.shift();
+
+    return arr;
+
   },
+
+
 
   concat: function(arr1, arr2) {
 
+   let arr3 = arr1.concat(arr2);
+
+   return arr3;
+
   },
+
+
 
   insert: function(arr, item, index) {
 
+    arr.splice(2,0,'z');
+
+    return arr;
+
   },
 
+
+
   count: function(arr, item) {
+
+    let number = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+
+      if (arr[i] === item) {
+
+      number = number+1;
+
+      }
+
+    }
+
+    return number;
 
   },
 
