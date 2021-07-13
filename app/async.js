95b6e619fdd9bbb4050e3f364fdac9353b825e7c
Cowlor <gillet.maxence.isart@gmail.com>
2021-07-13 16:43:47 +0200

    Update async.js

--- app/async.js
+++ app/async.js
@@ -3,6 +3,26 @@ exports = typeof window === 'undefined' ? global : window;
 exports.asyncAnswers = {
   async: function(value) {
 
+    let result;
+
+    let firstPromise = new Promise((resolve) => {
+      
+      setTimeout(() => {
+
+        resolve(value);
+
+      }, 100);
+
+    });
+
+    firstPromise.then((value) => {
+
+      console.log(value);
+
+    });
+
+    return firstPromise;
+
   },
 
   manipulateRemoteData: function(url) {
