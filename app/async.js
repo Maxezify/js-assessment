5dbf3bf0f28e2b3d02a9aeba31162e67913d462b
Cowlor <gillet.maxence.isart@gmail.com>
2021-07-13 19:41:52 +0200

    Update async.js

--- app/async.js
+++ app/async.js
@@ -15,17 +15,42 @@ exports.asyncAnswers = {
 
     });
 
-    firstPromise.then((value) => {
-
-      console.log(value);
-
-    });
-
     return firstPromise;
 
   },
 
   manipulateRemoteData: function(url) {
 
+    let request = new XMLHttpRequest();
+
+    let arr2 = [];
+    
+    request.open('GET', url);
+
+    request.responseType = 'json';
+
+    request.send();
+
+    request.onload = function() {
+
+      let elements = request.response;
+
+      for (let i = 0; i < elements.people.length; i++) {
+
+      let result = elements.people[i].name;
+
+      arr2.push(result);
+
+      }
+
+      arr2.sort();
+
+      console.log(arr2);
+
+      return arr2;
+
+    }
+
   }
+
 };
