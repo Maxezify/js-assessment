28f38d8dfe5638f67b1108d9da8fc7da9d92d6b9
Cowlor <gillet.maxence.isart@gmail.com>
2021-07-15 17:14:20 +0200

    Update count.js

--- app/count.js
+++ app/count.js
@@ -3,5 +3,31 @@ exports = typeof window === 'undefined' ? global : window;
 exports.countAnswers = {
   count: function (start, end) {
 
-  }
+    let timerMachine;
+
+    timerEngine();
+
+    function timerEngine() {
+
+      if (start < end) {
+
+      timerMachine = setTimeout(timerEngine, 100);
+
+      }
+
+     console.log(start++);
+
+    }
+
+    return {
+      cancel: function() {
+
+        clearTimeout(timerMachine);
+
+      }
+
+    };
+
+    }
+
 };
