09508a57a451ac4476258f49238f3d4f5cfc23d1
Cowlor <gillet.maxence.isart@gmail.com>
2021-07-15 18:41:30 +0200

    Update arrays.js

--- app/arrays.js
+++ app/arrays.js
@@ -61,6 +61,18 @@ if (Array.prototype.indexOf(2)) {
 
   removeWithoutCopy: function(arr, item) {
 
+    for (let i = 0; i < arr.length; i++) {
+
+      if (arr[i] === item) {
+
+        arr.splice(i, item);
+
+      }
+
+  }
+
+  return arr;
+
   },
 
   append: function(arr, item) {
