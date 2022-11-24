f6033e845683eed650251feb25eca5594c80a9ce
Maxence GILLET <gillet.maxence.isart@gmail.com>
2022-11-24 15:14:44 +0100

    numbers.js done

--- app/numbers.js
+++ app/numbers.js
@@ -1,24 +1,20 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.numbersAnswers = {
-  valueAtBit: function(num, bit) {
-
-  return 1 & (num >> (bit - 1));
-
+  valueAtBit: function (num, bit) {
+    return 1 & (num >> (bit - 1));
   },
 
-  base10: function(str) {
-
-  return parseInt(str, 2);
-
+  base10: function (str) {
+    return parseInt(str, 2);
   },
 
-  convertToBinary: function(num) {
-
+  convertToBinary: function (num) {
+    let binary = num.toString(2);
+    return "00000000".slice(0, -binary.length) + binary;
   },
 
-  multiply: function(a, b) {
-
-   
-  }
+  multiply: function (a, b) {
+    return (a * 10 * (b * 10)) / 100;
+  },
 };
